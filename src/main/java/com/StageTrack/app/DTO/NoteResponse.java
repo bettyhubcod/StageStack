f01b824469407f0248a0d8228bcdc1b17cbe08b5
bettyhubcod <ndiayebetty333@gmail.com>
2026-05-02 22:57:12 +0000

    code pour le create du crud note

--- src/main/java/com/StageTrack/app/DTO/NoteResponse.java
+++ src/main/java/com/StageTrack/app/DTO/NoteResponse.java
@@ -1,4 +1,19 @@
 package com.StageTrack.app.DTO;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
 public class NoteResponse {
+    private Long id;
+    private String fait;
+    private String appris;
+    private String probleme;
+    private String solution;
+    private String plus;
+    private LocalDate date;
+    private Long stageId;
 }
